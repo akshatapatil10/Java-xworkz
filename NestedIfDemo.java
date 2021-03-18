6c0df69b041971ce1aeb62b4b88b37496626f5fe
akshatapatil10 <80346414+akshatapatil10@users.noreply.github.com>
2021-03-18 10:06:17 +0530

    Add files via upload

---
+++ NestedIfDemo.java
@@ -0,0 +1,17 @@
+Class NestedIfDemo{
+public static void main(String[] args){
+boolean isEngineeringCompleted =True;
+boolean KnowJava = True;
+boolean KnowPython = False;
+if (is Engineering Completed){
+if (knowJava){
+System.out.println("Apply job for java Developer");
+}
+else if (KnowPython){
+System.out.println("Apply job for python Developer");
+}
+else{
+System.out.println("can't Apply job for Developer");
+}
+}
+}
